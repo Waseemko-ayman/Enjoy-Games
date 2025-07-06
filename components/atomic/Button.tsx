6f84f3c2ef2971778835f63ee5742c36953a2fcc
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-07-06 21:31:49 +0300

    Add a new color, light purple.

---
+++ components/atomic/Button.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  // variant: 'primary' | 'secondary' | 'outline' | 'cover' | 'circle';
+  variant: 'primary' | 'secondary';
+  borderRadius?: string;
+  otherClassName?: string;
+  handleClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  type?: 'submit' | 'button' | 'reset';
+  Icon?: React.ElementType;
+}
+
+const Button = ({
+  children,
+  variant,
+  borderRadius = 'rounded-4xl',
+  otherClassName = '',
+  type = 'submit',
+  handleClick,
+  Icon,
+}: ButtonProps) => {
+  return (
+    <button
+      className={`flex items-center justify-center gap-1 w-full text-white cursor-pointer text-base outline-none font-semibold transition-all duration-600 ${borderRadius} ${
+        variant === 'primary'
+          ? 'bg-daleel-primary hover:bg-daleel-primary-light'
+          : variant === 'secondary'
+          ? 'bg-daleel-secondary hover:bg-daleel-secondary-dark'
+          : ''
+      } ${otherClassName}`}
+      onClick={handleClick}
+      type={type}
+    >
+      {Icon && <Icon />}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
