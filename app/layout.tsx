90fa0b8d4d3d6b5aeb2d6800f9d19c333f96f79f
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-07-07 11:44:00 +0300

    Used dir in Layout temporarily

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" dir='rtl'>
       <head>
         <link rel="icon" href="/assets/logo.png" sizes="any" />
       </head>
