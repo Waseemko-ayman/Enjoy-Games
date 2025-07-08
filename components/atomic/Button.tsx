f118de2bdb16d0673d16af17b99408c75b01e4f5
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-07-08 17:18:31 +0300

    Improved Button Component and added a condition to show the position to icon before or after text button

--- components/atomic/Button.tsx
+++ components/atomic/Button.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 interface ButtonProps {
   children: React.ReactNode;
-  // variant: 'primary' | 'secondary' | 'outline' | 'cover' | 'circle';
-  variant: 'primary' | 'secondary';
+  variant: 'primary' | 'secondary' | 'third';
   borderRadius?: string;
   otherClassName?: string;
   handleClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   type?: 'submit' | 'button' | 'reset';
   Icon?: React.ElementType;
+  iconPosition?: 'left' | 'right';
 }
 
 const Button = ({
@@ -19,21 +19,25 @@ const Button = ({
   type = 'submit',
   handleClick,
   Icon,
+  iconPosition = 'left',
 }: ButtonProps) => {
   return (
     <button
-      className={`flex items-center justify-center gap-1 w-full text-white cursor-pointer text-lg outline-none font-semibold transition-all duration-600 ${borderRadius} ${
+      className={`flex items-center justify-center gap-1 text-white cursor-pointer text-lg outline-none font-semibold transition-all duration-600 ${borderRadius} ${
         variant === 'primary'
           ? 'bg-daleel-primary hover:bg-daleel-primary-light'
           : variant === 'secondary'
           ? 'bg-daleel-secondary hover:bg-daleel-secondary-light'
+          : variant === 'third'
+          ? 'bg-daleel-primary-deep hover:bg-daleel-primary-deep-light'
           : ''
       } ${otherClassName}`}
       onClick={handleClick}
       type={type}
     >
-      {Icon && <Icon />}
+      {iconPosition === 'left' && Icon && <Icon />}
       {children}
+      {iconPosition === 'right' && Icon && <Icon />}
     </button>
   );
 };
