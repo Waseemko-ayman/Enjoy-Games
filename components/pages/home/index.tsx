1b872b0be08e509a7564da888b56202ddb8fc756
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-07-08 17:27:02 +0300

    Used Banner Component in Home Page and Call to EnjoyWinWin and ServiceAdvantages

--- components/pages/home/index.tsx
+++ components/pages/home/index.tsx
@@ -5,10 +5,20 @@ import BestSellers from './Sections/BestSellers';
 import SuggestedProducts from './Sections/SuggestedProducts';
 import NewlyArrived from './Sections/NewlyArrived';
 import RedeemPoints from './Sections/RedeemPoints';
+import EnjoyWinWin from './Sections/EnjoyWinWin';
+import ServiceAdvantages from './Sections/ServiceAdvantages';
+import Banner from './Sections/Banner';
 
 const HomePage = () => {
   return (
     <>
+      <Banner
+        images={[
+          { src: '/assets/banners/banner1.webp', alt: 'Banner 1' },
+          { src: '/assets/banners/banner2.webp', alt: 'Banner 2' },
+          { src: '/assets/banners/banner3.webp', alt: 'Banner 3' },
+        ]}
+      />
       <Container>
         <SectionsTypes />
         <BestSellers />
@@ -16,6 +26,8 @@ const HomePage = () => {
         <NewlyArrived />
         <RedeemPoints />
       </Container>
+      <EnjoyWinWin />
+      <ServiceAdvantages />
     </>
   );
 };
