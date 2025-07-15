55a7d2212a58512fe6f0e508f9b4f1a429d93436
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-07-15 18:13:48 +0300

    Hero Banner Update

---
+++ components/pages/home/Sections/HeroBanner.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+/* eslint-disable @next/next/no-img-element */
+import { useState, useEffect } from 'react';
+
+interface BannerSlide {
+  id: number;
+  image: string;
+}
+
+const slides: BannerSlide[] = [
+  { id: 1, image: '/assets/banners/banner1.webp' },
+  { id: 2, image: '/assets/banners/banner2.webp' },
+  { id: 3, image: '/assets/banners/banner3.webp' },
+];
+
+export default function HeroBanner() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleNextSlide();
+    }, 5000);
+    return () => clearInterval(interval);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentSlide]);
+
+  const handleNextSlide = () => {
+    if (isTransitioning) return;
+    setIsTransitioning(true);
+    setTimeout(() => {
+      setCurrentSlide((prev) => (prev + 1) % slides.length);
+      setIsTransitioning(false);
+    }, 300);
+  };
+
+  const goToSlide = (index: number) => {
+    if (isTransitioning || index === currentSlide) return;
+    setIsTransitioning(true);
+    setTimeout(() => {
+      setCurrentSlide(index);
+      setIsTransitioning(false);
+    }, 300);
+  };
+
+  return (
+    <div className="relative w-full h-[40vh] overflow-hidden rounded-lg my-10">
+      {/* Background Images */}
+      <div className="absolute inset-0">
+        {slides.map((slide, index) => (
+          <div
+            key={slide.id}
+            className={`absolute inset-0 transition-opacity duration-500 ease-in-out ${
+              index === currentSlide ? 'opacity-100' : 'opacity-0'
+            }`}
+          >
+            <img
+              src={slide.image}
+              alt={`slide-${slide.id}`}
+              className="w-full h-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
+
+      {/* Slide Indicators */}
+      <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 flex space-x-2 z-10">
+        {slides.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => goToSlide(index)}
+            className={`w-3 h-3 rounded-full transition-all duration-300 ${
+              index === currentSlide
+                ? 'bg-white scale-125'
+                : 'bg-white/50 hover:bg-white/75'
+            }`}
+          />
+        ))}
+      </div>
+
+      {/* Progress Bar */}
+      <div className="absolute bottom-0 left-0 right-0 h-1 bg-white/20 z-10">
+        <div
+          className="h-full bg-enjoy-primary transition-all duration-300 ease-linear"
+          style={{
+            width: `${((currentSlide + 1) / slides.length) * 100}%`,
+          }}
+        />
+      </div>
+    </div>
+  );
+}
--- components/pages/home/index.tsx
+++ components/pages/home/index.tsx
@@ -6,19 +6,13 @@ import NewlyArrived from './Sections/NewlyArrived';
 import RedeemPoints from './Sections/RedeemPoints';
 import EnjoyWinWin from './Sections/EnjoyWinWin';
 import ServiceAdvantages from './Sections/ServiceAdvantages';
-import Banner from './Sections/Banner';
 import WalletSection from './Sections/WalletSection';
+import HeroBanner from './Sections/HeroBanner';
 
 const HomePage = () => {
   return (
     <>
-      <Banner
-        images={[
-          { src: '/assets/banners/banner1.webp', alt: 'Banner 1' },
-          // { src: '/assets/banners/banner2.webp', alt: 'Banner 2' },
-          // { src: '/assets/banners/banner3.webp', alt: 'Banner 3' },
-        ]}
-      />
+      <HeroBanner />
       <SectionsTypes />
       <WalletSection />
       <BestSellers />
