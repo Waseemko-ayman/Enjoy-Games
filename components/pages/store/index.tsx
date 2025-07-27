7fe6bce446680137d02c8dcc85d6180022ecf14d
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-07-27 20:10:41 +0300

    Randomly display subcategories on Store page from API

--- components/pages/store/index.tsx
+++ components/pages/store/index.tsx
@@ -1,7 +1,35 @@
-import CategoryCardsGrid from '@/components/organism/CategoryCardsGrid';
-import { shoppingCarts } from '@/data';
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client';
+import dynamic from 'next/dynamic';
+import useAPI from '@/hook/useAPI';
+import React, { useEffect } from 'react';
+import Loading from '@/components/molecules/loading';
+import { Category, SubCategories } from '@/interfaces';
+const CategoryCardsGrid = dynamic(
+  () => import('@/components/organism/CategoryCardsGrid'),
+  {
+    loading: () => <Loading />,
+  }
+);
 
-const StorePage = () => <CategoryCardsGrid cards={shoppingCarts} />;
+const StorePage = () => {
+  const { get, data: categories } = useAPI(`categories-subcategories`);
+
+  const shuffleArray = (array: SubCategories[]) => {
+    return array
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+  };
+
+  const allSubCategories = shuffleArray(
+    categories?.flatMap((cat: Category) => cat.sub_categories) || []
+  );
+
+  useEffect(() => {
+    get();
+  }, []);
+  return <CategoryCardsGrid cards={allSubCategories} />;
+};
 
 export default StorePage;
