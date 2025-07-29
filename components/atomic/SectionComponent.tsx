b85725efb95b07dcee4a3e117682fa63559c81f9
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-07-29 14:23:36 +0300

    Edit Title Size to SectionComponent

--- components/atomic/SectionComponent.tsx
+++ components/atomic/SectionComponent.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 import { SectionComponentProps } from '@/interfaces';
 import ResponsiveWrapper from '../molecules/ResponsiveWrapper';
 import AnimatedWrapper from '../molecules/FramerMotion/AnimatedWrapper';
+import useIsMobile from '@/hook/useIsMobile';
 
 const SectionComponent: React.FC<SectionComponentProps> = ({
   title,
   children,
 }) => {
+  const isMobile = useIsMobile();
   return (
     <ResponsiveWrapper>
       <AnimatedWrapper>
-        <h2 className="text-3xl font-semibold mb-6 px-4 lg:px-0">{title}</h2>
+        <h2
+          className={`${
+            isMobile ? 'text-2xl' : 'text-3xl'
+          } font-semibold mb-6 px-4 lg:px-0`}
+        >
+          {title}
+        </h2>
       </AnimatedWrapper>
       {children}
     </ResponsiveWrapper>
