bd675b51ad056cd26a06f4ac2fe45517fc1dd988
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-07-29 14:29:56 +0300

    Edit Title Size in WalletCard Comp.

--- components/molecules/WalletCard.tsx
+++ components/molecules/WalletCard.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { WalletCardProps } from '@/interfaces';
+import useIsMobile from '@/hook/useIsMobile';
 
 const WalletCard: React.FC<WalletCardProps> = ({
   title,
@@ -14,20 +15,25 @@ const WalletCard: React.FC<WalletCardProps> = ({
   pathName,
   isUnitTranslatable = false,
 }) => {
+  const isMobile = useIsMobile();
   const showIcon = (icon: React.ElementType) => {
     const Icon = icon;
     return <Icon className="w-7 h-7" />;
   };
+
+  const titleSize = isMobile ? 'text-lg' : 'text-xl';
   return (
     <Link
       href={pathName}
-      className={`rounded-2xl px-5 py-8 flex flex-col justify-between h-[120px] ${bgColor} ${textColor}`}
+      className={`rounded-2xl ${
+        isMobile ? 'p-5 h-[100px]' : 'px-5 py-8 h-[120px]'
+      } flex flex-col justify-between ${bgColor} ${textColor}`}
     >
       <div className="flex items-center gap-2">
         {icon && showIcon(icon)}
-        <h5 className="text-xl font-semibold">{title}</h5>
+        <h5 className={`${titleSize} font-semibold`}>{title}</h5>
       </div>
-      <div className="text-xl font-bold flex items-center gap-2 mt-2.5">
+      <div className={`${titleSize} font-bold flex items-center gap-2 mt-2.5`}>
         <span>{value}</span>
         {isUnitTranslatable ? (
           <span className="text-base">{unit}</span>
