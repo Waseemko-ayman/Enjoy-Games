2da6611d9374a3e1beb97f45f5629e9db3476d3e
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-08-07 14:15:58 +0300

    Edit Size to title and button in SubCartHeader in my cart pae

--- components/molecules/SubCartHeader.tsx
+++ components/molecules/SubCartHeader.tsx
@@ -17,15 +17,15 @@ const SubCartHeader = ({
 }) => {
   const { isArabic } = useToggleLocale();
   return (
-    <div className="flex justify-center items-center gap-4 mb-10 sm:justify-between sm:mb-8 flex-wrap ">
+    <div className="flex justify-center items-center gap-4 mb-10 sm:justify-between sm:mb-8 flex-wrap">
       <AnimatedWrapper direction="x" distance={40}>
-        <h1 className="text-2xl font-bold">{title}</h1>
+        <h1 className="text-xl sm:text-2xl font-bold">{title}</h1>
       </AnimatedWrapper>
       <AnimatedWrapper direction="x" distance={-40}>
         <Button
           variant="ghost"
           href={href}
-          otherClassName="border border-gray-700 !text-gray-700 hover:!bg-white px-6 py-3 w-full sm:w-[350px]"
+          otherClassName="border border-gray-700 !text-gray-700 hover:!bg-white px-5 py-2 sm:px-6 sm:py-3 w-full sm:w-[350px]"
           Icon={isArabic ? ArrowRight : ArrowLeft}
           handleClick={handleClick}
         >
