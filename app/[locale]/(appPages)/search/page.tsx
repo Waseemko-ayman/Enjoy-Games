0178b54832a86d1563eae546cfb8bb34946555b2
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-08-08 11:45:55 +0300

    Add params to generateMetadata in Search pages

--- app/[locale]/(appPages)/search/page.tsx
+++ app/[locale]/(appPages)/search/page.tsx
@@ -1,18 +1,14 @@
 import SearchPage from '@/components/pages/search';
-import { Metadata } from 'next';
 
 type Props = {
+  params: Record<string, string>;
   searchParams: { query?: string };
 };
 
-export async function generateMetadata({
-  searchParams,
-}: Props): Promise<Metadata> {
+export async function generateMetadata({ params, searchParams }: Props) {
   const query = searchParams.query || '';
   return {
-    title: query
-      ? `${query} | إنجوي قيمز`
-      : 'إنجوي قيمز',
+    title: query ? `${query} | إنجوي قيمز` : 'إنجوي قيمز',
   };
 }
 
