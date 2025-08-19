d23b6930ac3eac90c688b0cb50d11be313b0fa14
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-08-19 20:08:55 +0300

    Use extractText in ProductDetailsTabContent and wrap it with product?.description

--- components/pages/product/Sections/ProductDetailsTabContent.tsx
+++ components/pages/product/Sections/ProductDetailsTabContent.tsx
@@ -1,8 +1,13 @@
 import MotionSection from '@/components/molecules/FramerMotion/MotionSection';
 import { ProductCardProps } from '@/interfaces';
+import { extractText } from '@/utils/extractText';
 import React from 'react';
 
-const ProductDetailsTabContent = ({ product }: { product: ProductCardProps }) => {
+const ProductDetailsTabContent = ({
+  product,
+}: {
+  product: ProductCardProps;
+}) => {
   return (
     <div className="mt-3">
       <MotionSection index={1}>
@@ -12,7 +17,7 @@ const ProductDetailsTabContent = ({ product }: { product: ProductCardProps }) =>
       </MotionSection>
       <MotionSection index={2}>
         <p className="text-[15px] leading-6 text-right">
-          {product?.description}
+          {extractText(product?.description)}
         </p>
       </MotionSection>
     </div>
