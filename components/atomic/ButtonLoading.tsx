ee59b70c29ad44d694cbdf097c27974c978d1482
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-08-21 21:09:23 +0300

    Add borderColor Porps to ButtonLoading

--- components/atomic/ButtonLoading.tsx
+++ components/atomic/ButtonLoading.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const ButtonLoading = () => {
+const ButtonLoading = ({
+  borderColor = 'border-white',
+}: {
+  borderColor?: string;
+}) => {
   return (
-    <span className="animate-spin border-2 border-white border-t-transparent rounded-full w-4 h-4 inline-block" />
+    <span
+      className={`animate-spin border-2 ${borderColor} border-t-transparent rounded-full w-4 h-4 inline-block`}
+    />
   );
 };
 
