868ac6df3c98aa30856946aaeba7efb49ffad2f1
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-08-29 17:37:21 +0300

    Delete TotalPaid Context

--- components/pages/wallet/index.tsx
+++ components/pages/wallet/index.tsx
@@ -1,15 +1,14 @@
 'use client';
 import React from 'react';
 import PageHeader from '@/components/molecules/PageHeader';
-import { TotalPaidProvider } from '@/context/TotalPaidContext';
 import WalletContent from './Sections/WalletContent';
 
 const WalletPage = () => {
   return (
-    <TotalPaidProvider>
+    <>
       <PageHeader />
       <WalletContent />
-    </TotalPaidProvider>
+    </>
   );
 };
 
