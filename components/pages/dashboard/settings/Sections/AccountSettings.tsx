432f6d9ff5038ad32a9fa6897bc3f67b77e4cf0a
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-09-02 16:24:03 +0300

    Update the response for the AccountSettings Tab in the Settings Page in Dashboard and use loading and error to display user data.

--- components/pages/dashboard/settings/Sections/AccountSettings.tsx
+++ components/pages/dashboard/settings/Sections/AccountSettings.tsx
@@ -20,7 +20,6 @@ import Input from '@/components/atomic/Input';
 import { getInitials } from '@/utils/stringUtils';
 import { useUserInfo } from '@/context/UserInfoContext';
 import ButtonLoading from '@/components/atomic/ButtonLoading';
-import InlineError from '@/components/molecules/InlineError';
 import { useToast } from '@/lib/toast';
 import useAPI from '@/hook/useAPI';
 import {
@@ -30,6 +29,8 @@ import {
 } from '@/utils/accountSchema';
 
 import 'react-phone-number-input/style.css';
+import Loading from '@/components/molecules/loading';
+import ErrorFetching from '@/components/molecules/ErrorFetching';
 
 const AccountSettings = () => {
   const [avatarPreview, setAvatarPreview] = useState<string | null>(null);
@@ -48,7 +49,7 @@ const AccountSettings = () => {
   // Get User Information
   const {
     user,
-    isLoading: userInfoLoadinf,
+    isLoading: userInfoLoading,
     error: userInfoError,
   } = useUserInfo();
 
@@ -108,131 +109,133 @@ const AccountSettings = () => {
       description={t('Dashboard.settings.desc')}
     >
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col gap-6 sm:flex-row">
-          <div className="flex flex-col items-center gap-4">
-            <Avatar className="h-24 w-24 shadow-lg">
-              {userInfoLoadinf ? (
-                <ButtonLoading borderColor="text-black" />
-              ) : userInfoError ? (
-                <InlineError textColor="text-black" />
-              ) : (
-                <AvatarImage src={getPhotoSrc()} alt="character" />
-              )}
-              <AvatarFallback>{initials}</AvatarFallback>
-            </Avatar>
-            <input
-              type="file"
-              accept="image/*"
-              {...register('photo')}
-              ref={fileInputRef}
-              onChange={handleFileChange}
-              className="hidden"
-            />
-            <Button
-              type="button"
-              variant="outline"
-              size="sm"
-              onClick={() => fileInputRef.current?.click()}
-            >
-              <Upload className="mr-2 h-4 w-4" />
-              {t('BtnTexts.changeAvatar')}
-            </Button>
-            {errors.photo && (
-              <p className="text-sm text-red-500 mt-1">
-                {errors.photo.message}
-              </p>
-            )}
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full">
-            {inputData.map((input, index) => {
-              const label = t(`Inputs.labels.${input.name}`);
-              const placeholder =
-                t(`Inputs.placeHolders.${input.name}`) ||
-                input.placeholder ||
-                '';
+        {userInfoLoading ? (
+          <Loading />
+        ) : userInfoError ? (
+          <ErrorFetching />
+        ) : (
+          <>
+            <div className="flex flex-col gap-6 lg:flex-row">
+              <div className="flex flex-col items-center gap-4">
+                <Avatar className="h-24 w-24 shadow-lg">
+                  <AvatarImage src={getPhotoSrc()} alt="character" />
+                  <AvatarFallback>{initials}</AvatarFallback>
+                </Avatar>
+                <input
+                  type="file"
+                  accept="image/*"
+                  {...register('photo')}
+                  ref={fileInputRef}
+                  onChange={handleFileChange}
+                  className="hidden"
+                />
+                <Button
+                  type="button"
+                  variant="outline"
+                  size="sm"
+                  onClick={() => fileInputRef.current?.click()}
+                >
+                  <Upload className="mr-2 h-4 w-4" />
+                  {t('BtnTexts.changeAvatar')}
+                </Button>
+                {errors.photo && (
+                  <p className="text-sm text-red-500 mt-1">
+                    {errors.photo.message}
+                  </p>
+                )}
+              </div>
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full mb-5">
+                {inputData.map((input, index) => {
+                  const label = t(`Inputs.labels.${input.name}`);
+                  const placeholder =
+                    t(`Inputs.placeHolders.${input.name}`) ||
+                    input.placeholder ||
+                    '';
 
-              const options = input.options
-                ? input.options.map((opt) => ({
-                    ...opt,
-                    label:
-                      t(`Inputs.options.${input.name}.${opt.value}`) ||
-                      opt.labelKey,
-                  }))
-                : undefined;
+                  const options = input.options
+                    ? input.options.map((opt) => ({
+                        ...opt,
+                        label:
+                          t(`Inputs.options.${input.name}.${opt.value}`) ||
+                          opt.labelKey,
+                      }))
+                    : undefined;
 
-              return (
-                <AnimatedWrapper key={input.id} custom={index}>
-                  <div className="relative">
-                    {input.type === 'number' ? (
-                      <div className="w-full">
-                        <label className="block text-gray-700 font-normal text-sm mb-2">
-                          {label}
-                        </label>
-                        <Controller
-                          name={input.name as keyof FormValues}
-                          control={control}
-                          render={({ field: { onChange, value } }) => (
-                            <PhoneInput
-                              international
-                              defaultCountry="OM"
-                              value={
-                                typeof value === 'string' ? value : '' // Convert any non-string type to an empty string
-                              }
-                              onChange={onChange}
-                              placeholder={placeholder}
-                              className={`px-4 py-3 bg-white rounded-lg border border-gray-300 focus:outline-none focus:ring-5 focus:ring-[var(--enjoy-primary)]`}
-                              readOnly
+                  return (
+                    <AnimatedWrapper key={input.id} custom={index}>
+                      <div className="relative">
+                        {input.type === 'number' ? (
+                          <div className="w-full">
+                            <label className="block text-gray-700 font-normal text-sm mb-2">
+                              {label}
+                            </label>
+                            <Controller
+                              name={input.name as keyof FormValues}
+                              control={control}
+                              render={({ field: { onChange, value } }) => (
+                                <PhoneInput
+                                  international
+                                  defaultCountry="OM"
+                                  value={
+                                    typeof value === 'string' ? value : '' // Convert any non-string type to an empty string
+                                  }
+                                  onChange={onChange}
+                                  placeholder={placeholder}
+                                  className={`px-4 py-3 bg-white rounded-lg border border-gray-300 focus:outline-none focus:ring-5 focus:ring-[var(--enjoy-primary)]`}
+                                  readOnly
+                                />
+                              )}
                             />
-                          )}
-                        />
-                      </div>
-                    ) : (
-                      <div>
-                        <Input
-                          variant="secondary"
-                          type={input.type as InputTypes}
-                          label={label}
-                          placeholder={placeholder}
-                          Icon={input.icon}
-                          iconClassName="h-4 w-4 text-gray-400"
-                          inputName={input.name}
-                          options={options}
-                          otherClassNameContainer={
-                            errors[input.name as keyof FormValues]
-                              ? 'border-red-500'
-                              : ''
-                          }
-                          {...register(input.name as keyof FormValues)}
-                          readOnly={
-                            input.name === 'email' || input.name === 'phone'
-                          }
-                        />
-                        {errors[input.name as keyof FormValues] && (
-                          <FormError
-                            message={
-                              errors[input.name as keyof FormValues]
-                                ?.message as string
-                            }
-                          />
+                          </div>
+                        ) : (
+                          <div>
+                            <Input
+                              variant="secondary"
+                              type={input.type as InputTypes}
+                              label={label}
+                              placeholder={placeholder}
+                              Icon={input.icon}
+                              iconClassName="h-4 w-4 text-gray-400"
+                              inputName={input.name}
+                              options={options}
+                              otherClassNameContainer={
+                                errors[input.name as keyof FormValues]
+                                  ? 'border-red-500'
+                                  : ''
+                              }
+                              {...register(input.name as keyof FormValues)}
+                              readOnly={
+                                input.name === 'email' || input.name === 'phone'
+                              }
+                            />
+                            {errors[input.name as keyof FormValues] && (
+                              <FormError
+                                message={
+                                  errors[input.name as keyof FormValues]
+                                    ?.message as string
+                                }
+                              />
+                            )}
+                          </div>
                         )}
                       </div>
-                    )}
-                  </div>
-                </AnimatedWrapper>
-              );
-            })}
-          </div>
-        </div>
-        <Button type="submit">
-          {isLoading ? (
-            <>
-              {t('BtnTexts.Saving')}
-              <ButtonLoading />
-            </>
-          ) : (
-            t('BtnTexts.SaveChanges')
-          )}
-        </Button>
+                    </AnimatedWrapper>
+                  );
+                })}
+              </div>
+            </div>
+            <Button type="submit">
+              {isLoading ? (
+                <>
+                  {t('BtnTexts.Saving')}
+                  <ButtonLoading />
+                </>
+              ) : (
+                t('BtnTexts.SaveChanges')
+              )}
+            </Button>
+          </>
+        )}
       </form>
     </SettingsTab>
   );
